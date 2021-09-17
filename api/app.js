6f32957e50f3594125c67d2d09c8e812962105cb
roberto larrazabal <rlarrazabal90@gmail.com>
2021-09-17 16:58:53 -0400

    fix modulo 2

--- api/app.js
+++ api/app.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const pathToIndex = path.resolve(__dirname, '../client/index.html')
+const express = require('express');
+const path = require('path');
+const app = express();
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
 app.use('/*', (req, res) => {
-  res.sendFile(pathToIndex)
-})
-module.exports(app)
+  res.sendFile(pathToIndex);
+});
+module.exports = app;
