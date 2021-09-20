4b9c20f3dd21eed15b9cbdc4e7b70df1c1e80e6b
roberto larrazabal <rlarrazabal90@gmail.com>
2021-09-20 11:07:28 -0400

    module 4 image processor

--- api/src/resizeWorker.js
+++ api/src/resizeWorker.js
@@ -1,4 +1,3 @@
-
 const gm = require('gm');
 const {workerData, parentPort} = require('worker_threads');
 gm(workerData.source).resize(100, 100).write(workerData.destination,(error) => {
--- api/src/router.js
+++ api/src/router.js
@@ -5,6 +5,7 @@ const storage = multer.diskStorage(
     {destination: 'api/uploads/', filename});
 const upload = multer({fileFilter, storage});
 const path = require('path');
+const imageProcessor = require('./imageProcessor');
 const photoPath = path.resolve(__dirname,'../../client/photo-viewer.html');
 function filename(req, file, callback) {
     callback(null, file.originalname)
@@ -17,8 +18,14 @@ function fileFilter(req, file, callback) {
     callback(null, false, new Error('Wrong file type'));
   }
 }
-router.post('/upload', upload.single('photo'), (req, res) => {
+router.post('/upload', upload.single('photo'), 
+async (req, res) => {
     if(!req.fileValidationError) {
+        try {
+            await imageProcessor(req.file.filename);
+        } catch (error) {
+            res.status(500).json({error: error.message});
+        }
         res.status(201).json({success: true})
     } else {
         res.status(400).json({error: req.fileValidationError});
